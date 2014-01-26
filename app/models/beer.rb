03e21fcf4c69c0a8e1c0e34763846060c785cf58
Aurora Tulilaulu <tulilaulu@gmail.com>
2014-01-26 23:21:57 +0200

    week 2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings, dependent: :destroy
+
+	def to_s
+		return self.name+" by "+self.brewery.name
+	end
+
+	def average_rating
+		return self.ratings.average('score').to_s
+	end
 end
