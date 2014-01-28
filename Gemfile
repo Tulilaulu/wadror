2d20e88801c6383e5e52131a824ab003afd4d214
Aurora Tulilaulu <tulilaulu@gmail.com>
2014-01-28 15:08:45 +0200

    routes

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ group :production do
 end
 
 gem 'byebug', group: [:development, :test]
+gem 'bcrypt-ruby', '~> 3.1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ratebeer::Application.routes.draw do
+  resources :memberships
+
+  resources :beer_clubs
+
+  resources :users
+
   resources :beers
 
   resources :breweries
@@ -9,6 +15,12 @@ get 'ratings', to: 'ratings#index'
 get 'ratings/new', to:'ratings#new'
 post 'ratings', to: 'ratings#create'
 resources :ratings, only: [:index, :new, :create, :destroy]
+resources :sessions, only: [:new, :create, :destroy]
+get 'signup', to: 'users#new'
+get 'signin', to: 'sessions#new'
+delete 'signout', to: 'sessions#destroy'
+resources :memberships, only: [:index, :new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
