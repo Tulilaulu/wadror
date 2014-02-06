edb78cb6ff36c3799af10770ba8e98f0b2cb9f4f
Aurora Tulilaulu <tulilaulu@gmail.com>
2014-02-06 22:50:37 +0200

    testing started

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,9 +4,6 @@ include RatingAverage
 	has_many :ratings, dependent: :destroy
 	has_many :raters,-> { uniq }, through: :ratings, source: :user
 
- validates :score, numericality: { greater_than_or_equal_to: 1,
-                                    less_than_or_equal_to: 50,
-                                    only_integer: true }
 	validates :name, presence: true
 	def to_s
 		return self.name+" by "+self.brewery.name
